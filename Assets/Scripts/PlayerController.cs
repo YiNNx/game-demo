a9fd587c5900f47f5655731cd15c2d032c278d38
YiNN <1826630034@qq.com>
2022-06-22 08:54:44 +0800

    fix: movement bug

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,7 +9,9 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 6f;
     public Rigidbody2D rb;
     public Animator animator;
-    bool jumpPress;
+    bool isJump;
+    bool isMove;
+    float move;
     int jumpCount;
 
     // Start is called before the first frame update
@@ -21,40 +23,61 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        getStatus();
         Jump();
-        
+    }
+
+    void getStatus()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            isJump=true;
+        }else{
+            isJump=false;
+        }
+
+        move=Input.GetAxisRaw("Horizontal");
     }
 
     private void FixedUpdate() 
     {
         Move();
+        Anime();
     }
 
     void Move()
     {
-        float moveX=Input.GetAxisRaw("Horizontal");
-        if(moveX!=0){
-            transform.localScale = new Vector3(moveX,1,1);
-            animator.SetInteger("running",1);
-            Vector2 position = transform.position;
-            position.x+=moveX * speed * Time.deltaTime;
-            transform.position=position;
-        }else{
-            animator.SetInteger("running",0);
-
-        }
+        rb.velocity=new Vector2(speed*move,rb.velocity.y);
     }
 
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) )
+        if (isJump)
         {
             animator.SetInteger("jumpforward",1);
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }else{
             animator.SetInteger("jumpforward",0);
-
         }
+    }
+
+    void Anime(){
+
+        if(move!=0){
+            transform.localScale = new Vector3(move,1,1);
+            animator.SetInteger("running",1);
+        }else{
+            animator.SetInteger("running",0);
+        }
+
+        if (isJump)
+        {
+            animator.SetInteger("jumpforward",1);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }else{
+            animator.SetInteger("jumpforward",0);
+        }
+
     }
 
 }
