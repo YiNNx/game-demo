3fefc8d61d719eed8070d51d1e20a4bccc0b5f7d
Kiia <2301534957@qq.com>
2022-06-22 09:36:16 +0800

    new UI

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour
+{
+    public GameObject PauseMenu;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PauseGame()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+}
